b6cd5156641f3976e17ae4856aaa967915a563ac
Sandy Gomez <sgomez1029@gmail.com>
2024-04-26 16:36:13 -0700

    Initial Commit

---
+++ Lesson4AssignmentUCSDFutures/Lesson4AssignmentUCSDFuturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4AssignmentUCSDFuturesApp.swift
+//  Lesson4AssignmentUCSDFutures
+//
+//  Created by Sandra Gomez on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4AssignmentUCSDFuturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
