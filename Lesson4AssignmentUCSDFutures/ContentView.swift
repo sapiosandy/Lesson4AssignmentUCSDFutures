6bb77267b26a75e9d9aa43a68a79c00a9d4fe87e
Sandy Gomez <sgomez1029@gmail.com>
2024-04-26 20:09:24 -0700

    Lesson4AssignmentUCSDFutures

--- Lesson4AssignmentUCSDFutures/ContentView.swift
+++ Lesson4AssignmentUCSDFutures/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.blue.edgesIgnoringSafeArea(.all)
+                VStack {
+                    TextField("Type something here", text: $message)
+                        .padding()
+                        .textFieldStyle(.roundedBorder)
+                    
+                    NavigationLink(destination: SameFileView(passedData: message)) {
+                        Text("Second View")
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.top)
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct SameFileView: View {
+    var passedData: String
+    
+    var body: some View {
+        ZStack {
+            Color.gray.edgesIgnoringSafeArea(.all)
+            Text("This is what you typed: \(passedData)")
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
